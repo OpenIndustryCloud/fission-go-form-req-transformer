8415b5b2e17e8eb1b4c3d1fa3fa2f2704da67d75
Amit Varman <AmitKumarVarman@gmail.com>
2017-11-06 13:45:45 +0000

    IMAGE and Video URLs extracted in output and error handling improved

--- form-data-transformer.go
+++ form-data-transformer.go
@@ -9,6 +9,8 @@ request along side Weather API Input Data
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 	"strings"
@@ -16,44 +18,58 @@ import (
 )
 
 const (
+	BOOLEAN_FEILD = "boolean"
+	TEXT_FEILD    = "text"
+	DATE_FEILD    = "date"
+	FILE_UPLOAD   = "file_upload"
+	PARA_START    = "<p>"
+	PARA_END      = "</p><hr>"
+
+	//TV FIELD IDs
 	TV_CLAIM          = "TV claim"
-	STORM_SURGE_CLAIM = "Storm surge claim"
-	BOOLEAN_FEILD     = "boolean"
-	TEXT_FEILD        = "text"
-	DATE_FEILD        = "date"
-	PARA_START        = "<p>"
-	PARA_END          = "</p><hr>"
+	TV_INCIDENT_DATE  = "63241330"
+	TV_PURCHASE_PRICE = "63391165"
+	TV_CRIME_REF      = "AZbSqcXTKlED"
+	TV_MODEL_NO       = "lGCVB9tse6Re"
+	TV_MAKE           = "WwzqHPb0K9Wv"
+	TV_SERIAL_NO      = "u2dzBoYFjbRA"
+	TV_DAMAGE_IMAGE   = "63241151"
+	TV_RECIEPT        = "63241244"
+
+	//Storm Surge Field IDs
+	STORM_SURGE_CLAIM     = "Storm surge claim"
+	STORM_INCIDENT_DATE   = "nAz5fZvtiuLO"
+	STORM_INCIDENT_PLACE  = "64049754"
+	STORM_DAMAGE_IMAGE_1  = "63907299"
+	STORM_DAMAGE_IMAGE_2  = "j79cNctIvogK"
+	STORM_REPAIR_ESTIMATE = "wFpTHm7AZVNO"
+	STORM_SHORT_VIDEO     = "63907004"
 )
 
-func main() {
-	println("staritng app..")
-	http.HandleFunc("/", Handler)
-	http.ListenAndServe(":8083", nil)
-}
+// func main() {
+// 	println("staritng app..")
+// 	http.HandleFunc("/", Handler)
+// 	http.ListenAndServe(":8083", nil)
+// }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
-	if r.Body == nil {
-		http.Error(w, "Please send a valid JSON", 400)
-		return
-	}
 	//Marhsal TYPE FORM DATA to TypeFormData struct
 	var typeFormdata TypeFormData
 	err := json.NewDecoder(r.Body).Decode(&typeFormdata)
-	if err != nil {
-		http.Error(w, err.Error(), 400)
+	if err == io.EOF || err != nil {
+		createErrorResponse(w, err.Error(), 400)
 		return
 	}
 
+	//transform type form data manually
 	transformedData := transformData(typeFormdata)
-
 	transformedDataJSON, err := json.Marshal(&transformedData)
+	fmt.Println("Type form data after transformatin -----> ", string(transformedDataJSON))
 	if err != nil {
-		println(err)
+		createErrorResponse(w, err.Error(), 400)
 		return
 	}
-
-	println("ticket details - ", string(transformedDataJSON))
 
 	w.Header().Set("content-type", "application/json")
 	w.Write([]byte(string(transformedDataJSON)))
@@ -65,14 +81,14 @@ func transformData(typeFormdata TypeFormData) TranformedData {
 	var ticketDetails = TicketDetails{}
 	var transformedData = TranformedData{}
 
-	ticketDetails.Ticket.Subject = typeFormdata.FormResponse.Definition.Title
+	var claimType = typeFormdata.FormResponse.Definition.Title
 
-	//set requester details
+	//update Ticket details
+	ticketDetails.Ticket.Subject = claimType
 	ticketDetails.Ticket.Requester.Email = typeFormdata.FormResponse.Hidden.Email
 	ticketDetails.Ticket.Requester.Name = typeFormdata.FormResponse.Hidden.Name
 	ticketDetails.Ticket.Requester.Phone = typeFormdata.FormResponse.Hidden.Phone
 	ticketDetails.Ticket.Requester.PolicyNumber = typeFormdata.FormResponse.Hidden.Policy
-
 	ticketDetails.Ticket.Requester.LocaleID = 1
 
 	//default data
@@ -84,6 +100,7 @@ func transformData(typeFormdata TypeFormData) TranformedData {
 	//populate Descripton
 	for i, field := range typeFormdata.FormResponse.Definition.Fields {
 		answer := typeFormdata.FormResponse.Answers[i]
+		//add all data to ticket body
 		ticketBody += PARA_START + "<b>" + field.Title + "</b>"
 
 		switch field.Type {
@@ -91,26 +108,96 @@ func transformData(typeFormdata TypeFormData) TranformedData {
 			ticketBody += strconv.FormatBool(answer.Boolean) + PARA_END
 		case DATE_FEILD:
 			ticketBody += " : " + answer.Date + PARA_END
+		case FILE_UPLOAD:
+			ticketBody += field.ID + " : " + answer.FileURL + PARA_END
 		default:
 			ticketBody += " : " + answer.Text + PARA_END
 		}
-
 		ticketDetails.Ticket.Comment.HTMLBody = ticketBody
-
-		//for Storm Surge Claims - work around as ID is not returned by TYPE form
-		if strings.Compare(field.Title, STORM_SURGE_CLAIM) >= 0 {
-			if strings.Contains(field.Title, "Where did the incident happen? (City/town name)") {
-				transformedData.WeatherAPIInput.City = answer.Text
-			}
-			if strings.Contains(field.Title, "When did the incident happen?") {
+		//claim specific important data
+		if strings.Compare(claimType, STORM_SURGE_CLAIM) >= 0 {
+			//for Storm Surge Claims
+			switch field.ID {
+			case STORM_INCIDENT_DATE:
 				transformedData.WeatherAPIInput.Date = strings.Replace(answer.Date, "-", "", 2)
+				transformedData.StromClaimData.IncidentDate = answer.Date
+			case STORM_INCIDENT_PLACE:
+				transformedData.WeatherAPIInput.City = answer.Text
+				transformedData.StromClaimData.IncidentPlace = answer.Text
+			case STORM_DAMAGE_IMAGE_1:
+				transformedData.StromClaimData.DamageImageURL1 = answer.FileURL
+			case STORM_DAMAGE_IMAGE_2:
+				transformedData.StromClaimData.DamageImageURL2 = answer.FileURL
+			case STORM_REPAIR_ESTIMATE:
+				transformedData.StromClaimData.RepairEstimateImage = answer.FileURL
+			case STORM_SHORT_VIDEO:
+				transformedData.StromClaimData.DamageVideoURL = answer.FileURL
+			default:
+			}
+		} else {
+			//assuming TV claim request
+			//for Storm Surge Claims
+			switch field.ID {
+			case TV_INCIDENT_DATE:
+				transformedData.WeatherAPIInput.Date = strings.Replace(answer.Date, "-", "", 2)
+				transformedData.TVClaimData.IncidentDate = answer.Date
+			case TV_PURCHASE_PRICE:
+				transformedData.WeatherAPIInput.City = answer.Text
+				transformedData.TVClaimData.TVPrice = answer.Text
+			case TV_CRIME_REF:
+				transformedData.TVClaimData.CrimeRef = answer.Text
+			case TV_MODEL_NO:
+				transformedData.TVClaimData.TVModelNo = answer.Text
+			case TV_MAKE:
+				transformedData.TVClaimData.TVMake = answer.Text
+			case TV_SERIAL_NO:
+				transformedData.TVClaimData.TVSerialNo = answer.Text
+			case TV_DAMAGE_IMAGE:
+				transformedData.TVClaimData.DamageImageURL1 = answer.FileURL
+			case TV_RECIEPT:
+				transformedData.TVClaimData.TVReceiptImage = answer.FileURL
+			default:
 			}
 		}
+		//update ticket specific data
+		transformedData.TicketDetails = ticketDetails
 	}
-	//update ticket specific data
-	transformedData.TicketDetails = ticketDetails
-
 	return transformedData
+}
+
+type Error struct {
+	Code    int64  `json:"status"`
+	Message string `json:"message"`
+}
+
+//output data
+type TranformedData struct {
+	TicketDetails   TicketDetails   `json:"ticketDetails,omitempty"`
+	WeatherAPIInput WeatherAPIInput `json:"weatherAPIInput,omitempty"`
+	TVClaimData     TVClaimData     `json:"tv_claim_data,omitempty"`
+	StromClaimData  StromClaimData  `json:"strom_claim_data,omitempty"`
+}
+
+type TVClaimData struct {
+	TVPrice         string `json:"tv_price,omitempty"`
+	CrimeRef        string `json:"crime_ref,omitempty"`
+	IncidentDate    string `json:"incident_date,omitempty"`
+	TVModelNo       string `json:"tv_model_no,omitempty"`
+	TVMake          string `json:"tv_make,omitempty"`
+	TVSerialNo      string `json:"tv_serial_no,omitempty"`
+	DamageImageURL1 string `json:"damage_image_url_1,omitempty"`
+	DamageImageURL2 string `json:"damage_image_url_2,omitempty"`
+	TVReceiptImage  string `json:"tv_reciept_image_url"`
+	DamageVideoURL  string `json:"damage_video_url,omitempty"`
+}
+
+type StromClaimData struct {
+	IncidentPlace       string `json:"incident_place,omitempty"`
+	IncidentDate        string `json:"incident_date,omitempty"`
+	DamageImageURL1     string `json:"damage_image_url_1,omitempty"`
+	DamageImageURL2     string `json:"damage_image_url_2,omitempty"`
+	RepairEstimateImage string `json:"estimate_image_url,omitempty"`
+	DamageVideoURL      string `json:"damage_video_url,omitempty"`
 }
 
 //Model for TYPE FORM
@@ -175,12 +262,13 @@ type TicketDetails struct {
 		Priority string `json:"priority"`
 		Status   string `json:"status"`
 		Comment  struct {
-			HTMLBody string `json:"html_body"`
+			HTMLBody string   `json:"html_body"`
+			Uploads  []string `json:"uploads"`
 		} `json:"comment"`
 		CustomFields []struct {
 			ID    int    `json:"id"`
 			Value string `json:"value"`
-		} `json:"custom_fields"`
+		} `json:"custom_fields,omitempty"`
 		Requester struct {
 			LocaleID     int    `json:"locale_id"`
 			Name         string `json:"name"`
@@ -190,13 +278,18 @@ type TicketDetails struct {
 		} `json:"requester"`
 	} `json:"ticket"`
 }
+
 type WeatherAPIInput struct {
-	City    string `json:"city"`
-	Country string `json:"country"`
-	Date    string `json:"date"`
+	City    string `json:"city,omitempty"`
+	Country string `json:"country,omitempty"`
+	Date    string `json:"date,omitempty"` //YYYYMMDD
 }
 
-type TranformedData struct {
-	TicketDetails   TicketDetails   `json:"ticketDetails"`
-	WeatherAPIInput WeatherAPIInput `json:"weatherAPIInput"`
+func createErrorResponse(w http.ResponseWriter, message string, status int64) {
+	errorJSON, _ := json.Marshal(&Error{
+		Code:    status,
+		Message: message})
+	//Send custom error message to caller
+	w.Header().Set("content-type", "application/json")
+	w.Write([]byte(errorJSON))
 }
